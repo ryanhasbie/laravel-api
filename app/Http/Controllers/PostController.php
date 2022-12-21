52b7b9eeb25d50c1b83523ff203b0a55b02390e5
Ryan Hasbie <ryanhasbie7@gmail.com>
2022-12-21 08:29:03 +0700

    get all posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        return response()->json([
+            'posts' => Post::get(),
+        ]);
+    }
+}
