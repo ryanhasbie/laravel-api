856f5e8a320a17f985e792b05b5717cff74f58cb
Ryan Hasbie <ryanhasbie7@gmail.com>
2022-12-22 16:20:16 +0700

    authentication with laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post:id}', [PostController::class, 'show']);
+Route::get('/posts/{post:id}', [PostController::class, 'show'])->middleware('auth:sanctum');
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthenticationController::class, 'me'])->middleware('auth:sanctum');
+
+
