204059547fec2ec82b9b57f65bf373143b8779f9
Ryan Hasbie <ryanhasbie7@gmail.com>
2022-12-22 12:37:35 +0700

    create show method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ class Post extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected $with = ['user'];
 
     public function user() {
         return $this->belongsTo(User::class);
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
